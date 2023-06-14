f7a1ee333f88a63eb1e3c980e0b5e59f9f8cf2db
szmanda <michal.szmanda@student.put.poznan.pl>
2023-06-14 20:32:01 +0200

    Updating lamport before sending ACK
    Prevoiusly, lamport was updated later, which caused race conditions with sending a REQUST

--- watek_komunikacyjny.c
+++ watek_komunikacyjny.c
@@ -10,15 +10,18 @@ void *startKomWatek(void *ptr)
     /* Obrazuje pętlę odbierającą pakiety o różnych typach */
     while ( stan!=InFinish ) {
 	debug("czekam na recv");
-        MPI_Recv( &pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        switch ( status.MPI_TAG ) {
+    MPI_Recv( &pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    switch ( status.MPI_TAG ) {
 	    case REQUEST: 
-                debug("%d prosi o dostęp (z zegarem %d). Ja mam %d. Czy ubiegam się o dostęp? %d", pakiet.src, pakiet.ts, lamport, stan == InWant);
-                if (stan != InSection && (stan != InWant || pakiet.ts < lamport || (pakiet.ts == lamport && pakiet.src < rank))) {
-		                sendPacket( 0, status.MPI_SOURCE, ACK );
-                } else {
-                    debug("Nie odsyłam ACK do %d!", pakiet.src);
-                }
+            debug("%d prosi o dostęp (z zegarem %d). Ja mam %d. Czy ubiegam się o dostęp? %d", pakiet.src, pakiet.ts, lamport, stan == InWant);
+            if (stan != InSection && (stan != InWant || pakiet.ts < lamport || (pakiet.ts == lamport && pakiet.src < rank))) {
+                pthread_mutex_lock(&lamport_lock);
+                lamport = lamport < pakiet.ts ? pakiet.ts + 1 : lamport + 1; // before sending a new message, lamport update is required
+                pthread_mutex_unlock(&lamport_lock);
+                sendPacket( 0, status.MPI_SOURCE, ACK );
+            } else {
+                debug("Nie odsyłam ACK do %d!", pakiet.src);
+            }
 	    break;
 	    case ACK: 
 	        ackCount++; /* czy potrzeba tutaj muteksa? Będzie wyścig, czy nie będzie? Zastanówcie się. */
